e85fc7156595422960ef9f66f8e47c97ccff5915
Danilo Vitor <danilofenoninho65@gmail.com>
2026-08-02 00:50:30 -0300

    Construindo Layout do projeto

--- projeto-GoTask/src/app/app.component.ts
+++ projeto-GoTask/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from "./components/main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
